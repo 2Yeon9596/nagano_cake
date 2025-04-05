b1a64edce2fc14d0d9e14cbce06c9e62e8f05be6
출구없정연이의입구 <jeongyeononly1101@chulgueobsjeong-yeon-iuiibgunoMacBook-Air.local>
2025-04-06 00:13:09 +0900

    商品登録機能作成

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,16 +1,26 @@
 class Admin::ItemsController < ApplicationController
   before_action :authenticate_admin!
-
   def new
+    @item = Item.new
+    @genres = Genre.all
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      flash[:notice] = "商品が登録されました。"
+      redirect_to admin_item_path(@item)
+    else
+      @genres = Genre.all
+      render :new  
+    end  
   end
 
   def index
   end
-
-  def create
-  end
   
   def show
+    @item = Item.find(params[:id])
   end
   
   def edit
@@ -19,6 +29,10 @@ class Admin::ItemsController < ApplicationController
   def update
   end  
     
-    
-    
+  private
+  
+  def item_params
+    params.require(:item).permit(:name, :introduction, :price, :is_active, :genre_id, :image)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  has_one_attached :image
+  belongs_to :genre, dependent: :destroy
+
+  TAX_RATE = 0.10 #税率10%
+
+  def price_with_tax
+    (price * (1 + TAX_RATE)).floor
+  end  
 end
